6fa0940ddb6a2afdb04f6dd954f2d3e5140eb53f
Leo <lssants7@gmail.com>
2024-02-12 22:38:55 -0300

    updated script (functionalized)

---
+++ src/nn_architecture.py
@@ -0,0 +1,32 @@
+# ------------------- Define Neural Network architecture -------------------
+import torch.nn as nn
+
+nodes_config = [40, 40, 40, 40, 40]
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.dropout = nn.Dropout(0.2)
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(4, 40),
+            nn.ReLU(),
+            nn.Linear(40, 40),
+            nn.ReLU(),
+            nn.Linear(40, 40),
+            nn.ReLU(),
+            nn.Linear(40, 40),
+            nn.ReLU(),
+            nn.Linear(40, 40),
+            nn.ReLU(),
+            nn.Linear(40, 40),
+            nn.ReLU(),
+            nn.Linear(40, 1),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.dropout(x)
+        logits = self.linear_relu_stack(x)
+        return logits
