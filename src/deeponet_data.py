9815f4987d1d8f49e235ec21639cd687df8aaa9f
lsantia9 <l_santiago_benta_dos_santos@brown.edu>
2024-08-14 11:49:20 -0400

    code for dataset for test is ready

--- src/deeponet_data.py
+++ src/deeponet_data.py
@@ -6,6 +6,24 @@ project_dir = os.path.dirname(script_dir)
 sys.path.insert(0, project_dir)
 path_to_data = os.path.join(project_dir, 'data')
 
+
+def G(a, b, y):
+    c = (a/b).reshape(-1,1) # (n, 1) -> broadcasting row wise
+    return c*np.sin(b*y.T)
+
+def preprocess_data(u, y, Guy):
+    branch_input = np.repeat(u, repeats=len(y), axis=0)
+    trunk_input = np.tile(y, (len(u), 1))
+    output = Guy.flatten().reshape(-1,1)
+    return (branch_input, trunk_input, output)
+
+def split_data(arr):
+    train_spilt = 0.7
+    size = len(arr)
+    sample = int(train_spilt*size)
+    train_data, test_data = arr[:sample, :], arr[sample:, :]
+    return train_data, test_data
+
 np.random.seed(42)
 
 n = 100 # Number of input functions
@@ -20,23 +38,22 @@ end = 1000
 # ------- Branch input ------
 x = np.linspace(start, end, m) # sensors (dont necessarily have to be on a lattice - can be random - linspace is not required)
 a , b = 10*np.random.rand(n).reshape(n, 1), 10*np.random.rand(n).reshape(n, 1) 
-
 u = np.array([a[i] * np.cos(b[i]*x) for i in range(n)]) # function is u_n = a_n * cos(b_n*x)
 
 # ------- Trunk input -------
 y = end *np.random.rand(q).reshape(q, 1)
 
-
-def G(a, b, y):
-    c = (a/b).reshape(-1,1) # (n, 1) -> broadcasting row wise
-    return c*np.sin(b*y.T)
-
-# outputs
+# ------- Output -----------
 G_u_y = G(a, b, y)
 
-data = (u, y, G_u_y)
+
+# Split training and test set
+data = np.concatenate(preprocess_data(u, y, G_u_y), axis=1)
+antiderivative_train, antiderivative_test = split_data(data)
+
+print(f"Train size: {antiderivative_train.shape}, \nTest size: {antiderivative_test.shape}")
+
 
 if __name__ == '__main__':
-    np.save(os.path.join(path_to_data, 'branch_input.npy'), u)
-    np.save(os.path.join(path_to_data, 'trunk_input.npy'), y)
-    np.save(os.path.join(path_to_data, 'network_output.npy'), G_u_y)
+    np.save(os.path.join(path_to_data, 'antiderivative_train.npy'), antiderivative_train)
+    np.save(os.path.join(path_to_data, 'antiderivative_train.npy'), antiderivative_train)
