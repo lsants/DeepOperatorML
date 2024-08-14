155d1a5434cd2bd8003ff810039a37fc79d0d7be
lsantia9 <l_santiago_benta_dos_santos@brown.edu>
2024-08-14 19:06:47 -0400

    deeopnet data

--- src/deeponet_data.py
+++ src/deeponet_data.py
@@ -6,6 +6,7 @@ project_dir = os.path.dirname(script_dir)
 sys.path.insert(0, project_dir)
 path_to_data = os.path.join(project_dir, 'data')
 
+np.random.seed(42)
 
 def G(a, b, y):
     c = (a/b).reshape(-1,1) # (n, 1) -> broadcasting row wise
@@ -24,16 +25,12 @@ def split_data(arr):
     train_data, test_data = arr[:sample, :], arr[sample:, :]
     return train_data, test_data
 
-np.random.seed(42)
-
+# ----------- Set size of dataset and operator domain -----------
 n = 100 # Number of input functions
 m = 80 # Number of sensors (must be fixed)
 q = 60 # Output locations (can be random)
-p = 50 # Dimension of net's basis
-
 start = 0
 end = 1000
-
 
 # ------- Branch input ------
 x = np.linspace(start, end, m) # sensors (dont necessarily have to be on a lattice - can be random - linspace is not required)
@@ -46,14 +43,13 @@ y = end *np.random.rand(q).reshape(q, 1)
 # ------- Output -----------
 G_u_y = G(a, b, y)
 
-
-# Split training and test set
+# _------ Split training and test set --------
 data = np.concatenate(preprocess_data(u, y, G_u_y), axis=1)
 antiderivative_train, antiderivative_test = split_data(data)
 
 print(f"Train size: {antiderivative_train.shape}, \nTest size: {antiderivative_test.shape}")
 
-
+# --- Save dataset ---
 if __name__ == '__main__':
-    np.save(os.path.join(path_to_data, 'antiderivative_train.npy'), antiderivative_train)
-    np.save(os.path.join(path_to_data, 'antiderivative_test.npy'), antiderivative_test)
+    np.savez(os.path.join(path_to_data, 'antiderivative_train.npz'), X_branch=antiderivative_train[:,:m], X_trunk=antiderivative_train[:,m], y=antiderivative_train[:,m+1])
+    np.savez(os.path.join(path_to_data, 'antiderivative_test.npz'), X_branch=antiderivative_test[:,:m], X_trunk=antiderivative_test[:,m], y=antiderivative_test[:,m+1])
