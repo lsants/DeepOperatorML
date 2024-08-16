5c768eaf9aeb78b98743b6fc845134ed997ab4f2
lsantia9 <l_santiago_benta_dos_santos@brown.edu>
2024-08-16 19:40:11 -0400

    reorganize repo

--- src/generate_poly_dataset.py
+++ src/generate_poly_dataset.py
@@ -7,6 +7,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 project_dir = os.path.dirname(script_dir)
 sys.path.insert(0, project_dir)
 
+import generic as gnc
 
 class PolyDataset(Dataset):
     def __init__(self, data, transform=None):
@@ -64,11 +65,41 @@ if __name__ == '__main__':
     X, y = generate_data(n)
     poly_data = np.concatenate((X, y), axis=1)
     print('Dataset:', '\n', poly_data[:5], '\n')
-    np.save(os.path.join(data_path, 'poly_data'), poly_data)
 
     alpha, beta, gamma, B = X.T
     y = np.expand_dims(alpha/3 * B**3 + beta/2 * B**2 + gamma * B, axis=1)
     X = X[:, :-1]
-    poly_data = np.concatenate((X, y), axis=1)
+    poly_data = np.concatenate((X, y), axis=1).astype(np.float64)
     print('Fixed dataset:', '\n', poly_data[:5], '\n')
-    np.save(os.path.join(data_path, 'poly_data_fixed'), poly_data)
+
+    X, y = poly_data[:, :-1], poly_data[:, -1]
+    data_norm = np.zeros((len(poly_data), 5))
+    data_norm[:, :-1] = gnc.normalize(X)
+    data_norm[:, -1] = y
+
+    # --------------------- Format as torch and split train-test set ---------------------
+    training_data_params, val_data_params, test_data_params = gnc.split_dataset(data, seed=42)
+    mean_for_normalization, std_for_normalization = np.mean(training_data_params, axis=0)[:-1], np.std(training_data_params, axis=0)[:-1]
+
+    training_data, val_data, test_data = gnc.split_dataset(data_norm, seed=42)
+
+    for i in [training_data, val_data, test_data]:
+        i = i[:, :-1], i[:, -1]  # format for dataset
+
+    data_norm = data_norm[:, :-1], data_norm[:, -1]  # format for dataset
+
+    X_train, y_train = training_data[:, :-1], training_data[:, -1]
+    X_val, y_val = val_data[:, :-1], val_data[:, -1]
+    X_test, y_test = test_data[:, :-1], test_data[:, -1]
+    X_test_mean, X_test_std = np.mean(X_test, axis=0), np.std(X_test, axis=0)
+    y_test_mean, y_test_std = np.mean(y_test, axis=0), np.std(y_test, axis=0)
+
+    training_data_tensor = PolyDataset(
+        training_data, transform=ToTensor())
+    val_data_tensor = PolyDataset(val_data, transform=ToTensor())
+    test_data_tensor = PolyDataset(test_data, transform=ToTensor())
+    X, y = PolyDataset(data_norm, transform=ToTensor()).features, PolyDataset(
+        data_norm, transform=ToTensor()).labels
+    print(X.shape, y.shape)
+
+    np.savez(os.path.join(data_path, 'poly_dataset.npz'), train=training_data_tensor, val=val_data_tensor, test=test_data_tensor)
