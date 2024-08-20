6b38d4f8d9c002f636d7a4445a73503684848eac
lsantia9 <l_santiago_benta_dos_santos@brown.edu>
2024-08-20 11:40:15 -0400

    add default initialization for model

--- src/deeponet_architecture.py
+++ src/deeponet_architecture.py
@@ -2,24 +2,26 @@ import torch
 import torch.nn as nn
 
 class FNNDeepOnet(nn.Module):
-    def __init__(self, branch_layers, trunk_layers):
+    def __init__(self, branch_layers=None, trunk_layers=None):
         super().__init__()
         
         self.branch_layers = nn.ModuleList()
-        for i in range(len(branch_layers) - 1):
-            in_features = branch_layers[i]
-            out_features = branch_layers[i+1]
-            self.branch_layers.append(nn.Linear(in_features, out_features))
-            if i < len(branch_layers) - 2:
-                self.branch_layers.append(nn.ReLU())
+        if branch_layers is not None:
+            for i in range(len(branch_layers) - 1):
+                in_features = branch_layers[i]
+                out_features = branch_layers[i+1]
+                self.branch_layers.append(nn.Linear(in_features, out_features))
+                if i < len(branch_layers) - 2:
+                    self.branch_layers.append(nn.ReLU())
 
         self.trunk_layers = nn.ModuleList()
-        for i in range(len(trunk_layers) - 1):
-            in_features = trunk_layers[i]
-            out_features = trunk_layers[i+1]
-            self.trunk_layers.append(nn.Linear(in_features, out_features))
-            if i < len(trunk_layers) - 2:
-                self.trunk_layers.append(nn.ReLU())
+        if trunk_layers is not None:
+            for i in range(len(trunk_layers) - 1):
+                in_features = trunk_layers[i]
+                out_features = trunk_layers[i+1]
+                self.trunk_layers.append(nn.Linear(in_features, out_features))
+                if i < len(trunk_layers) - 2:
+                    self.trunk_layers.append(nn.ReLU())
 
     def branch(self, X):
         b = X
