ffed41c8ee2528c9e7cabb93bee52f51ebd3a2b3
lsantia9 <l_santiago_benta_dos_santos@brown.edu>
2024-10-23 21:10:37 -0400

    plotting scripts

---
+++ plot_labels.py
@@ -0,0 +1,47 @@
+import yaml
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+from modules.plotting import plot_label_axis, plot_label_contours
+
+f_index = 0
+
+date = datetime.today().strftime('%Y%m%d')
+
+with open('params_model.yaml') as file:
+    p_preds = yaml.safe_load(file)
+
+with open('data_generation_params.yaml') as file:
+    p_labels = yaml.safe_load(file)
+
+normalized = p_labels['non_dim']
+
+preds_datafile = p_preds["PREDS_DATA_FILE"]
+
+if p_preds['DEBUG']:
+    preds_datafile = preds_datafile[:-4] + "_" + date + '.npz'
+
+# ----------------Get Labels -----------------
+data_labels = np.load(p_labels["SAVED_DATA_PATH"], allow_pickle=True)
+
+omega = data_labels['freqs']                # Shape: (num_freqs,)
+r_label = data_labels['r_field']            # Shape: (n,)
+z_label = data_labels['z_field']            # Shape: (m,)
+wd = data_labels['wd']                      # Shape: (num_freqs, n, m)
+
+
+f_label_index = int(p_preds['TRAIN_PERC']*len(omega)) + f_index
+f_label = omega[f_label_index]
+
+r, z = r_label, z_label
+freq = f_label
+
+wd_plot = wd[f_label_index]
+
+
+fig = plot_label_contours(r, z, wd_plot, freq, full=True, non_dim_plot=p_labels['non_dim'])
+plt.show()
+
+fig = plot_label_axis(r, z, wd_plot, freq, axis=p_labels['axis_plot'], non_dim_plot=p_labels['non_dim'])
+
+plt.show()
---
+++ plot_preds.py
@@ -0,0 +1,58 @@
+import yaml
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+from modules.plotting import plot_label_axis, plot_label_contours
+
+f_index = 0
+
+date = datetime.today().strftime('%Y%m%d')
+
+with open('params_model.yaml') as file:
+    p_preds = yaml.safe_load(file)
+
+with open('data_generation_params.yaml') as file:
+    p_labels = yaml.safe_load(file)
+
+normalized = p_labels['non_dim']
+
+preds_datafile = p_preds["PREDS_DATA_FILE"]
+
+if p_preds['DEBUG']:
+    preds_datafile = preds_datafile[:-4] + "_" + date + '.npz'
+
+# ----------------Get Preds -----------------
+data_preds = np.load(preds_datafile, allow_pickle=True)
+
+u = data_preds["u"].flatten()                                # Shape: (test_size, 1)
+xt = data_preds["xt"]                              # Shape: (n*m, 2)
+g_u_real = data_preds["real"]                      # Shape: (test_size, n*m)
+g_u_imag = data_preds["imag"]                      # Shape: (test_size, n*m)
+mu_u = data_preds['mu_u']                          # Shape: (1,)
+sd_u = data_preds['sd_u']                          # Shape: (1,)
+mu_xt = data_preds['mu_xt']                        # Shape: (2,)
+sd_xt  = data_preds['sd_xt']                       # Shape: (2,)
+
+r_pred, z_pred = (xt[:,0][:p_labels['n_r']]*sd_xt[0] + mu_xt[0]), (np.unique(xt[:,1])*sd_xt[1] + mu_xt[1])
+
+f_pred_index = f_index
+
+real_u = u*sd_u + mu_u
+f_pred = real_u[f_pred_index].item()
+
+g_u = g_u_real + g_u_imag*1j
+g_u = g_u.reshape(-1, len(r_pred), len(z_pred))
+
+r, z = r_pred, z_pred 
+freq = f_pred
+g_u_plot = g_u[f_pred_index]
+
+print(freq)
+
+
+fig = plot_label_contours(r, z, g_u_plot, freq, full=True, non_dim_plot=p_labels['non_dim'])
+plt.show()
+
+fig = plot_label_axis(r, z, g_u_plot, freq, axis=p_labels['axis_plot'], non_dim_plot=p_labels['non_dim'])
+
+plt.show()
