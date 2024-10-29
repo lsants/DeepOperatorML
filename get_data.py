0c34e822bbe574fdc5876647b4fda109614d27fd
lsantia9 <l_santiago_benta_dos_santos@brown.edu>
2024-10-29 18:05:44 -0400

    new get_data main script with oop

--- get_data.py
+++ get_data.py
@@ -1,105 +1,46 @@
 import os
+import sys
 import yaml
 import numpy as np
-import matplotlib.pyplot as plt
-from tqdm.auto import tqdm
-from data_generation.influence import influence
-from modules.plotting import plot_label_contours, plot_label_axis
+import argparse
+from modules.evaluate_params import multi_eval as evaluate
+from data_generation.data_generation_dimless_green import DimensionlessInfluenceFunction
 
-## ------------- Get parameters --------------
 with open('data_generation_params.yaml') as file:
-    p  =  yaml.safe_load(file)
+    p = yaml.safe_load(file)
 
-filename = p['SAVED_DATA_PATH']
-seed = p['seed']
-non_dim = p['non_dim']
-np.random.seed(seed)
+np.random.seed(p["SEED"])
 
-print(filename)
+filename = os.path.join(f"{p['DATA_FILENAME']}")
 
-# ------- Material --------
-n = p['n_r']
-m = p['n_z']
-Es = eval(p['E'])
-vs = p['poisson']
-e1 = Es/(1+vs)/(1-2*vs)
-c11 = e1*(1-vs)
-c12 = e1*vs
-c13 = e1*vs
-c33 = e1*(1-vs)
-c44 = e1*(1-2*vs)/2
-damp = p['damp']
-dens =  p['dens']
+parser = argparse.ArgumentParser()
 
-# ----------- Load ------------
-z_source = p['z_source']
-r_source = p['r_source']
-l_source = p['l_source']
-loadmag = p['load']
-num_freqs = p['n_freq']
-f_min, f_max = p['freq_min'], p['freq_max']
-if isinstance(f_min, str) or isinstance(f_max, str):
-    f_min, f_max = eval(f_min), eval(f_max)
-freqs  =  f_min + np.random.rand(num_freqs) * (f_max - f_min)
-q0 = loadmag / (np.pi*r_source**2)
-scaling_factor = (q0 * r_source/c44)
+parser.add_argument("problem", type=str, help="Generate data for given problem")
+args = parser.parse_args()
 
-print(f"Load of {q0} Pa")
+problem = args.problem.lower()
 
-# ----------- Problem -----------
-bvptype = p['bvptype']
-loadtype = p['loadtype']
-component = p['component']
+# --------- Grouping parameters -------------
+data_size = (p["N"], p["N_R"], p["N_Z"])
+load_params = evaluate((p["OMEGA_MAX"], p["OMEGA_MIN"], p["LOAD"], p["Z_SOURCE"], p["L_SOURCE"], p["R_SOURCE"]))
+mesh_params = evaluate((p["R_MIN"], p["R_MAX"], p["Z_MIN"], p["Z_MAX"]))
+problem_setup = (p['COMPONENT'], p['LOADTYPE'], p['BVPTYPE'])
 
-# ------------ Mesh --------------
-r_max = p['r_max']
-z_max = p['z_max']
-r_min = p['r_min']*r_source
-z_min = p['z_min']
-r_field = np.linspace(r_min, r_max, n)
-z_field = np.linspace(z_min, z_max, m)
+if problem == "dimensionless":
+    E = (p["E"])
+    nu = (p["NU"])
+    damp = (p["DAMP"])
+    dens = (p["DENS"])
+    material_params = evaluate((E, nu, damp, dens))
 
-wd = np.zeros((num_freqs, n, m), dtype=complex)
+    influence_functions = DimensionlessInfluenceFunction(
+        data_size,
+        material_params,
+        load_params,
+        mesh_params,
+        problem_setup,
+    )
+    influence_functions.produce_samples(filename)
 
-# Modify if computing dimensionless displacement
-if non_dim:
-    scaling_factor = q0
-
-freqs_norm = freqs*r_source*np.sqrt(dens/c44)
-c11 = c11/c44
-c12 = c12/c44
-c13 = c13/c44
-c33 = c33/c44
-c44 = c44/c44
-dens = dens/dens
-r_source = r_source / p['r_source']
-z_source = z_source / p['r_source']
-r_field_norm = r_field / p['r_source']
-z_field_norm = z_field / p['r_source']
-
-## -------------- Calling function ----------------
-for i in tqdm(range(len(freqs_norm)), colour='Green'):
-    for j in range(len(r_field_norm)):
-        for k in range(len(z_field_norm)):
-            wd[i, j, k] =  scaling_factor * influence(
-                            c11, c12, c13, c33, c44,
-                            dens, damp,
-                            r_field_norm[j], z_field_norm[k],
-                            z_source, r_source, l_source,
-                            freqs_norm[i],
-                            bvptype, loadtype, component
-                        )
-
-try:
-    directory = os.path.dirname(filename)
-    os.makedirs(directory, exist_ok=True)
-except FileExistsError as e:
-    print('Rewriting previous data file...')
-
-R, Z = r_field, z_field
-
-if non_dim:
-    R, Z = R / p['r_source'], Z / p['r_source']
-    freqs = freqs_norm
-
-np.savez(filename, freqs=freqs, r_field=R, z_field=Z, wd=wd)
+else:
+    print("fatal error: not a valid problem.", file=sys.stderr)
