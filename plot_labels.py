9bc262dd2b7f91c493e54892c9a7e0f3622822e1
lsantia9 <l_santiago_benta_dos_santos@brown.edu>
2024-10-30 15:33:50 -0400

    updated plotting scripts

--- plot_labels.py
+++ plot_labels.py
@@ -14,20 +14,18 @@ with open('params_model.yaml') as file:
 with open('data_generation_params.yaml') as file:
     p_labels = yaml.safe_load(file)
 
-normalized = p_labels['non_dim']
 
 preds_datafile = p_preds["PREDS_DATA_FILE"]
 
-if p_preds['DEBUG']:
-    preds_datafile = preds_datafile[:-4] + "_" + date + '.npz'
-
 # ----------------Get Labels -----------------
-data_labels = np.load(p_labels['SAVED_DATA_PATH'], allow_pickle=True)
+data_labels = np.load(p_labels['DATA_FILENAME'], allow_pickle=True)
 
-omega = data_labels['freqs']                # Shape: (num_freqs,)
-r_label = data_labels['r_field']            # Shape: (n,)
-z_label = data_labels['z_field']            # Shape: (m,)
-wd = data_labels['wd']                      # Shape: (num_freqs, n, m)
+print(f"Plotting data from: {p_labels['DATA_FILENAME']}")
+
+omega = data_labels['xb']                # Shape: (num_freqs,)
+r_label = data_labels['r']            # Shape: (n,)
+z_label = data_labels['z']            # Shape: (m,)
+wd = data_labels['g_u']                      # Shape: (num_freqs, n, m)
 
 print('omega shape:', omega.shape)
 print('r labels shape:', r_label.shape)
@@ -44,9 +42,9 @@ freq = f_label
 wd_plot = wd[f_label_index]
 
 
-fig = plot_label_contours(r, z, wd_plot, freq, full=True, non_dim_plot=p_labels['non_dim'])
+fig = plot_label_contours(r, z, wd_plot, freq, full=True, non_dim_plot=True)
 plt.show()
 
-fig = plot_label_axis(r, z, wd_plot, freq, axis=p_labels['axis_plot'], non_dim_plot=p_labels['non_dim'])
+fig = plot_label_axis(r, z, wd_plot, freq, non_dim_plot=True)
 
 plt.show()
