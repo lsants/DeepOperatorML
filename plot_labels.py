94a2563e915329ba8f58d2f84f94e98d99172ab3
lsantia9 <l_santiago_benta_dos_santos@brown.edu>
2024-11-08 18:36:31 -0500

    plot labels now plot all frequencies in axis z=0 and r=0. animations for field plots to be added.

--- plot_labels.py
+++ plot_labels.py
@@ -5,47 +5,35 @@ from datetime import datetime
 from modules import dir_functions
 from modules import preprocessing as ppr
 from modules.greenfunc_dataset import GreenFuncDataset
-from modules.plotting import plot_axis, plot_field
-
-f_index = 0
-
-date = datetime.today().strftime('%Y%m%d')
+from modules.plotting import plot_axis, plot_field, plot_labels_axis
 
 with open('params_test.yaml') as file:
     p = yaml.safe_load(file)
 
 # ----------------Get Labels -----------------
 data = np.load(p['DATAFILE'], allow_pickle=True)
-dataset = GreenFuncDataset(data)
-indices = dir_functions.load_indices(p['INDICES_FILE'])
-norm_params = dir_functions.load_indices(p['NORM_PARAMS_FILE'])
 
 print(f"Plotting data from: {p['DATAFILE']}")
-print(f"Using indices from: {p['INDICES_FILE']}")
 
+dataset = GreenFuncDataset(data)
+indices = dir_functions.load_data_info(p['INDICES_FILE'])
 test_indices = indices['test']
 test_dataset = dataset[test_indices]
 
+xb = test_dataset['xb']
 xt = dataset.get_trunk()
 r, z = ppr.trunk_to_meshgrid(xt)
 
-xb = test_dataset['xb']
 g_u_real = test_dataset['g_u_real']
 g_u_imag = test_dataset['g_u_imag']
-
 g_u = g_u_real + g_u_imag * 1j
 g_u = ppr.reshape_from_model(g_u, z)
 
-print(g_u.shape)
+quantiles = np.quantile(xb, np.arange(0.1, 1.1, 0.1)) # Plot each 10th quantile frequency
 
-f_label = xb[f_index]
+mask = [(np.abs(xb - i)).argmin() for i in quantiles]
 
-freq = f_label.item()
-
-wd_plot = g_u[f_index]
-
-fig = plot_field(r, z, wd_plot, freq, full=False)
+fig_labels = plot_labels_axis(r, z, g_u[mask], xb[mask], non_dim_plot=True)
 plt.show()
 
-fig = plot_axis(r, z, wd_plot, freq)
-plt.show()
+# Add animations for field and axis plots
