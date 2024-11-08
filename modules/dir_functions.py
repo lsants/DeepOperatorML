944556a4d202203d48a17b4907359d67768c659a
lsantia9 <l_santiago_benta_dos_santos@brown.edu>
2024-11-08 18:10:15 -0500

    renamed function for json load

--- modules/dir_functions.py
+++ modules/dir_functions.py
@@ -9,7 +9,7 @@ def load_params(file):
         p = yaml.safe_load(file)
     return p
 
-def load_indices(file):
+def load_data_info(file):
     with open(file, 'r') as file:
         i = json.load(file)
         return i
