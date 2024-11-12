4646d7296b0864541c4d77f79d2d9de56f9fc755
lsantia9 <l_santiago_benta_dos_santos@brown.edu>
2024-11-12 17:59:50 -0500

    add chebyshev kan implementation

---
+++ modules/kan.py
@@ -0,0 +1,51 @@
+import torch
+
+class ChebyshevKANLayer(torch.nn.Module):
+    def __init__(self, input_dim, output_dim, degree):
+        super(ChebyshevKANLayer, self).__init__()
+        self.inputdim = input_dim
+        self.outdim = output_dim
+        self.degree = degree
+        self.eps = 1e-7
+        self.cheby_coeffs = torch.nn.Parameter(torch.empty(input_dim, output_dim, degree + 1))
+        self.register_buffer("arange", torch.arange(0, degree + 1, 1))
+
+        torch.nn.init.normal_(self.cheby_coeffs, mean=0.0, std=1 / (input_dim * (degree + 1)))
+
+    def forward(self, x):
+        x = torch.tanh(x)
+        x = x.view((-1, self.inputdim, 1)).expand(
+            -1, -1, self.degree + 1
+        )
+
+        torch.clip(x, -1.+self.eps, 1-self.eps)
+        x = x.acos()
+        x *= self.arange
+        x = x.cos()
+        y = torch.einsum(
+            "bid,iod->bo", x, self.cheby_coeffs
+        )
+        y = y.view(-1, self.outdim)
+
+        return y
+    
+class ChebyshevKAN(torch.nn.Module):
+    def __init__(self, layers, degree):
+        super(ChebyshevKAN, self).__init__()
+        self.linears = torch.nn.ModuleList()
+        self.degree = degree
+        num_layers = len(layers)
+        for layer_index in range(num_layers - 1):
+            self.linears.append(
+                ChebyshevKANLayer(
+                    layers[layer_index],
+                    layers[layer_index + 1],
+                    self.degree
+                ),
+            )
+            self.linears.append(torch.nn.LayerNorm(layers[layer_index + 1]))
+
+    def forward(self, x):
+        for layer_index in range(len(self.linears) - 1):
+            x = self.linears[layer_index](x)
+        return x
