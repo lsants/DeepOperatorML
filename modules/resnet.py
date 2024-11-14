8f50ee3f068c3d444bdea50bcc8c6ae2b5f63c3a
lsantia9 <l_santiago_benta_dos_santos@brown.edu>
2024-11-14 17:22:31 -0500

    fixed resnet

--- modules/resnet.py
+++ modules/resnet.py
@@ -5,23 +5,24 @@ class ResNet(torch.nn.Module):
         super(ResNet, self).__init__()
         self.linears = torch.nn.ModuleList()
         self.activation = activation
+        for i in range(len(layers) - 1):
+            self.linears.append(torch.nn.Linear(layers[i], layers[i + 1]))
+        self.activation = activation
 
-        num_layers = len(layers)
-        for layer_index in range(num_layers - 1):
-            self.linears.append(
-                torch.nn.Linear(
-                    layers[layer_index],
-                    layers[layer_index + 1]
-                )
-            )
-    
     def forward(self, inputs):
         out = inputs
-        num_layers = len(self.linears)
-        for layer_index in range(num_layers - 1):
-            layer_input = out
-            out = self.linears[layer_index](out)
-            out = self.activation(out)
-            if layer_index > 0 and layer_index % 2 == 0:
-                out = out + layer_input
-        return self.linears[-1](out)
+        num_linears = len(self.linears)
+
+        out = self.linears[0](out)
+        out = self.activation(out)
+        residual = out
+
+        for i in range(1, num_linears):
+            out = self.linears[i](out)
+            if i < num_linears - 1:
+                out = self.activation(out)
+            if i % 2 == 1:
+                if residual.shape == out.shape:
+                    out = out + residual
+                residual = out
+        return out
