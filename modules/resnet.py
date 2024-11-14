89679f632228483fb5a7e2fca9e4de793cccaaa9
lsantia9 <l_santiago_benta_dos_santos@brown.edu>
2024-11-14 18:47:44 -0500

    modularized resnet

--- modules/resnet.py
+++ modules/resnet.py
@@ -1,28 +1,40 @@
 import torch
 
+class ResidualBlock(torch.nn.Module):
+    def __init__(self, in_features, out_features, activation):
+        super(ResidualBlock, self).__init__()
+        self.activation = activation
+        self.linear1 = torch.nn.Linear(in_features, out_features)
+        self.linear2 = torch.nn.Linear(out_features, out_features)
+        self.shortcut = None
+        if in_features != out_features:
+            self.shortcut = torch.nn.Linear(in_features, out_features)
+
+    def forward(self, x):
+        identity = x
+        if self.shortcut is not None:
+            identity = self.shortcut(x)
+        out = self.linear1(x)
+        out = self.activation(out)
+        out = self.linear2(out)
+        out += identity
+        out = self.activation(out)
+        return out
+
 class ResNet(torch.nn.Module):
     def __init__(self, layers, activation):
         super(ResNet, self).__init__()
-        self.linears = torch.nn.ModuleList()
         self.activation = activation
-        for i in range(len(layers) - 1):
-            self.linears.append(torch.nn.Linear(layers[i], layers[i + 1]))
-        self.activation = activation
+        self.blocks = torch.nn.ModuleList()
+        num_blocks = len(layers) - 1
+
+        for i in range(num_blocks):
+            in_features = layers[i]
+            out_features = layers[i + 1]
+            self.blocks.append(ResidualBlock(in_features, out_features, activation))
 
     def forward(self, inputs):
         out = inputs
-        num_linears = len(self.linears)
-
-        out = self.linears[0](out)
-        out = self.activation(out)
-        residual = out
-
-        for i in range(1, num_linears):
-            out = self.linears[i](out)
-            if i < num_linears - 1:
-                out = self.activation(out)
-            if i % 2 == 1:
-                if residual.shape == out.shape:
-                    out = out + residual
-                residual = out
+        for block in self.blocks:
+            out = block(out)
         return out
