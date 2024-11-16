47236323d74f866c7c4204dc81d0d196812a4c43
lsantia9 <l_santiago_benta_dos_santos@brown.edu>
2024-11-16 00:51:20 -0500

    updated trainer class for two step (NOT WORKING FOR TWO STEP, finish it)

--- modules/training.py
+++ modules/training.py
@@ -2,10 +2,11 @@ import torch
 from .loss_complex import loss_complex
 
 class TrainModel:
-    def __init__(self, model, optimizer, scheduler=None):
+    def __init__(self, model, optimizer, scheduler=None, training_phase='both'):
         self.model = model
         self.optimizer = optimizer
         self.scheduler = scheduler
+        self.training_phase = training_phase
     
     def __call__(self, sample, val=False):
         if not val:
@@ -21,13 +22,22 @@ class TrainModel:
 
     def train_step(self, sample):
         self.model.train()
-        xb, xt = sample['xb'], sample['xt']
+        xb, xt = sample.get('xb'), sample.get('xt')
         g_u_real, g_u_imag = sample['g_u_real'], sample['g_u_imag']
 
         def closure():
             self.optimizer.zero_grad()
-            pred_real, pred_imag = self.model(xb, xt)
-            loss = loss_complex(g_u_real, g_u_imag, pred_real, pred_imag)
+            if self.training_phase == 'trunk':
+                basis_real, basis_imag = self.model(xt=xt)
+                loss = loss_complex(g_u_real, g_u_imag, basis_real, basis_imag)
+            elif self.training_phase == 'branch':
+                pred_real, pred_imag = self.model(xb=xb)
+                coefs_real = self.model.R @ self.model.A_real
+                coefs_imag = self.model.R @ self.model.A_imag
+                loss = loss_complex(coefs_real, coefs_imag, pred_real, pred_imag)
+            else:
+                pred_real, pred_imag = self.model(xb=xb, xt=xt)
+                loss = loss_complex(g_u_real, g_u_imag, pred_real, pred_imag)
             loss.backward()
             return loss
         
@@ -38,14 +48,20 @@ class TrainModel:
 
     def val_step(self, sample):
         self.model.eval()
-        xb, xt = sample['xb'], sample['xt']
+        xb, xt = sample.get('xb'), sample.get('xt')
         g_u_real, g_u_imag = sample['g_u_real'], sample['g_u_imag']
 
         with torch.no_grad():  
-            pred_real, pred_imag = self.model(xb, xt)
-            loss = loss_complex(g_u_real, g_u_imag, pred_real, pred_imag)
+            if self.training_phase == 'trunk':
+                basis_real, basis_imag = self.model(xt=xt)
+                loss = loss_complex(g_u_real, g_u_imag, basis_real, basis_imag)
+            elif self.training_phase == 'branch':
+                pred_real, pred_imag = self.model(xb=xb)
+                coefs_real = self.model.R @ self.model.A_real
+                coefs_imag = self.model.R @ self.model.A_imag
+                loss = loss_complex(coefs_real, coefs_imag, pred_real, pred_imag)
+            else:
+                pred_real, pred_imag = self.model(xb=xb, xt=xt)
+                loss = loss_complex(g_u_real, g_u_imag, pred_real, pred_imag)
 
         return loss.item(), pred_real, pred_imag
-    
-    def change_optimizer(self, new_optimizer):
-        self.optimizer = new_optimizer
