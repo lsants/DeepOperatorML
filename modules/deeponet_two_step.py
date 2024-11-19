84df90aa453eba83a3655f0358371fb3dfc2f3fc
lsantia9 <l_santiago_benta_dos_santos@brown.edu>
2024-11-19 17:51:50 -0500

    rewrote matrix prods to match paper

--- modules/deeponet_two_step.py
+++ modules/deeponet_two_step.py
@@ -20,12 +20,13 @@ class DeepONetTwoStep(DeepONet):
     def forward(self, xb=None, xt=None):
         if self.training_phase == 'trunk':
             trunk_out = self.trunk_network(xt)
-            basis_real = self.A_list[0] @ (trunk_out.T)
-            basis_imag = self.A_list[1] @ (trunk_out.T)
+            basis_real = trunk_out @ self.A_list[0]
+            basis_imag = trunk_out @ self.A_list[1]
             return basis_real, basis_imag
         
         elif self.training_phase == 'branch':
             branch_out = self.branch_network(xb)
+            print(branch_out.shape, "<----- branch output shape before transposing")
             if self.R is None:
                 raise ValueError("Basis functions have not been computed. Train trunk first.")
             return branch_out
@@ -36,26 +37,18 @@ class DeepONetTwoStep(DeepONet):
             branch_real_out = branch_out[ : , : num_basis]
             branch_imag_out = branch_out[ : , num_basis : ]
 
-            real_out = torch.matmul(branch_real_out, torch.transpose(trunk_out, 0, 1))
-            imag_out = torch.matmul(branch_imag_out, torch.transpose(trunk_out, 0, 1))
+            real_out = torch.matmul(trunk_out, torch.transpose(branch_real_out, 0, 1))
+            imag_out = torch.matmul(trunk_out, torch.transpose(branch_imag_out, 0, 1))
 
             return real_out, imag_out
-        
-    def freeze_branch(self):
-        for param in self.branch_network.parameters():
-            param.requires_grad = False
-
-    def unfreeze_branch(self):
-        for param in self.branch_network.parameters():
-            param.requires_grad = True
-    
-    def freeze_trunk(self):
-        for param in self.trunk_network.parameters():
-            param.requires_grad = False
-
-    def unfreeze_trunk(self):
-        for param in self.trunk_network.parameters():
-            param.requires_grad = True
     
     def set_training_phase(self, phase):
         self.training_phase = phase
+    
+    def freeze_A(self):
+        for A in self.A_list:
+            A.requires_grad = False
+
+    def unfreeze_A(self):
+        for A in self.A_list:
+            A.requires_grad = True
