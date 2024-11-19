0412da3ee164fe39f216a3aa0268e38e257a91fb
lsantia9 <l_santiago_benta_dos_santos@brown.edu>
2024-11-18 23:41:12 -0500

    modifying data for plot (unfinished)

--- modules/train_evaluator.py
+++ modules/train_evaluator.py
@@ -3,13 +3,13 @@ import torch
 class TrainEvaluator:
     def __init__(self, error_norm):
         self.error_norm = error_norm
-        self.train_loss_history = []
-        self.val_loss_history = []
-        self.train_real_error_history = []
-        self.train_imag_error_history = []
-        self.val_real_error_history = []
-        self.val_imag_error_history = []
-        self.learning_rate_history = []
+        self.train_loss_history = None
+        self.val_loss_history = None
+        self.train_real_error_history = None
+        self.train_imag_error_history = None
+        self.val_real_error_history = None
+        self.val_imag_error_history = None
+        self.learning_rate_history = None
 
     def compute_error(self, g_u, pred):
         with torch.no_grad():
@@ -18,24 +18,38 @@ class TrainEvaluator:
         return error.detach().cpu().numpy()
     
     def store_epoch_train_loss(self, epoch_loss):
+        if not self.train_loss_history:
+            self.train_loss_history = []
         self.train_loss_history.append(epoch_loss)
 
     def store_epoch_val_loss(self, epoch_loss):
+        if not self.val_loss_history:
+            self.val_loss_history = []
         self.val_loss_history.append(epoch_loss)
     
     def store_epoch_train_real_error(self, epoch_error):
+        if not self.train_real_error_history:
+            self.train_real_error_history = []
         self.train_real_error_history.append(epoch_error)
     
     def store_epoch_train_imag_error(self, epoch_error):
+        if not self.train_imag_error_history:
+            self.train_imag_error_history = []
         self.train_imag_error_history.append(epoch_error)
     
     def store_epoch_val_real_error(self, epoch_error):
+        if not self.val_real_error_history:
+            self.val_real_error_history = []
         self.val_real_error_history.append(epoch_error)
     
     def store_epoch_val_imag_error(self, epoch_error):
+        if not self.val_imag_error_history:
+            self.val_imag_error_history = []
         self.val_imag_error_history.append(epoch_error)
 
     def store_epoch_learning_rate(self, learning_rate):
+        if not self.learning_rate_history:
+            self.learning_rate_history = []
         self.learning_rate_history.append(learning_rate)
 
     def get_loss_history(self):
