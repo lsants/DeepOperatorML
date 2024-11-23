3e56ed6b459f98a549671e268a525cb5855e48a0
lsantia9 <l_santiago_benta_dos_santos@brown.edu>
2024-11-23 06:49:40 -0500

    this class calls everything needed during training

--- modules/training.py
+++ modules/training.py
@@ -1,106 +1,184 @@
+# modules/training_loop.py
+
+import time
 import torch
-from .loss_complex import loss_complex
+from . import preprocessing as ppr
+from .saving import Saver
+from tqdm.auto import tqdm
+from .plotting import plot_training
+from .train_evaluator import TrainEvaluator
 
-class ModelTrainer:
-    def __init__(self, model, optimizer, scheduler=None):
+class TrainingLoop:
+    def __init__(self, model, training_strategy, evaluator, saver, params):
+        """
+        Initializes the TrainingLoop.
+
+        Args:
+            model (torch.nn.Module): The model to train.
+            training_strategy (TrainingStrategy): The training strategy to use.
+            evaluator (TrainEvaluator): The evaluator for metrics.
+            saver (Saver): The saver for saving models and outputs.
+            params (dict): Training parameters.
+        """
         self.model = model
-        self.optimizer = optimizer
-        self.scheduler = scheduler
-    
-    def __call__(self, sample, val=False):
-        if not val:
-            loss, pred_real, pred_imag = self.train_step(sample)
+        self.training_strategy = training_strategy
+        self.evaluator = evaluator
+        self.saver = saver
+        self.p = params
+
+        self.training_strategy.prepare_training(self.model)
+
+        self.optimizers = self.training_strategy.get_optimizers(self.model, self.p)
+        self.schedulers = self.training_strategy.get_schedulers(self.optimizers, self.p)
+
+    def prepare_batch(self, batch):
+        """
+        Prepares the batch data, including normalization and feature expansion.
+
+        Args:
+            batch (dict): The batch data.
+
+        Returns:
+            dict: The processed batch data.
+        """
+        processed_batch = {}
+        dtype = getattr(torch, self.p['PRECISION'])
+        device = self.p['DEVICE']
+
+        xb_scaler = ppr.Scaling(
+            min_val=self.p['NORMALIZATION_PARAMETERS']['xb']['min'],
+            max_val=self.p['NORMALIZATION_PARAMETERS']['xb']['max']
+        )
+        xt_scaler = ppr.Scaling(
+            min_val=self.p['NORMALIZATION_PARAMETERS']['xt']['min'],
+            max_val=self.p['NORMALIZATION_PARAMETERS']['xt']['max']
+        )
+        g_u_real_scaler = ppr.Scaling(
+            min_val=self.p['NORMALIZATION_PARAMETERS']['g_u_real']['min'],
+            max_val=self.p['NORMALIZATION_PARAMETERS']['g_u_real']['max']
+        )
+        g_u_imag_scaler = ppr.Scaling(
+            min_val=self.p['NORMALIZATION_PARAMETERS']['g_u_imag']['min'],
+            max_val=self.p['NORMALIZATION_PARAMETERS']['g_u_imag']['max']
+        )
+
+        if self.p['INPUT_NORMALIZATION']:
+            processed_batch['xb'] = xb_scaler.normalize(batch['xb']).to(dtype=dtype, device=device)
+            processed_batch['xt'] = xt_scaler.normalize(batch['xt']).to(dtype=dtype, device=device)
         else:
-            loss, pred_real, pred_imag = self.val_step(sample)
+            processed_batch['xb'] = batch['xb'].to(dtype=dtype, device=device)
+            processed_batch['xt'] = batch['xt'].to(dtype=dtype, device=device)
 
-        outputs = {'loss': loss,
-                    'pred_real': pred_real,
-                    'pred_imag': pred_imag}
-        
-        return outputs
-
-    def train_step(self, sample):
-        self.model.train()
-        xb, xt = sample.get('xb'), sample.get('xt')
-        g_u_real, g_u_imag = sample['g_u_real'], sample['g_u_imag']
-
-        self.optimizer.zero_grad()
-        if xt is None:
-            pred_real, pred_imag = self.model(xb=xb)
-            pred_real, pred_imag = pred_real.T, pred_imag.T
+        if self.p['OUTPUT_NORMALIZATION']:
+            processed_batch['g_u_real'] = g_u_real_scaler.normalize(batch['g_u_real']).to(dtype=dtype, device=device)
+            processed_batch['g_u_imag'] = g_u_imag_scaler.normalize(batch['g_u_imag']).to(dtype=dtype, device=device)
         else:
-            pred_real, pred_imag = self.model(xb=xb, xt=xt)
-        loss = loss_complex(g_u_real, g_u_imag, pred_real, pred_imag)
-        loss.backward()
-        
-        self.optimizer.step()
+            processed_batch['g_u_real'] = batch['g_u_real'].to(dtype=dtype, device=device)
+            processed_batch['g_u_imag'] = batch['g_u_imag'].to(dtype=dtype, device=device)
 
-        return loss.item(), pred_real, pred_imag
+        if self.p['TRUNK_FEATURE_EXPANSION']:
+            processed_batch['xt'] = ppr.trunk_feature_expansion(
+                processed_batch['xt'], self.p['TRUNK_EXPANSION_FEATURES_NUMBER']
+            )
 
-    def val_step(self, sample):
-        self.model.eval()
-        xb, xt = sample.get('xb'), sample.get('xt')
-        g_u_real, g_u_imag = sample['g_u_real'], sample['g_u_imag']
+        return processed_batch
+    def train(self, train_batch, val_batch=None):
+        """
+        Perform the training loop.
 
-        with torch.no_grad():  
-            if xt is None:
-                pred_real, pred_imag = self.model(xb=xb)
-                pred_real, pred_imag = pred_real.T, pred_imag.T
+        Args:
+            train_batch (dict): The training data as a dictionary.
+            val_batch (dict, optional): The validation data as a dictionary.
+        """
+        epochs = self.p['N_EPOCHS']
+        best_avg_error_real = float('inf')
+        best_model_checkpoint = None
+
+        start_time = time.time()
+
+        for epoch in tqdm(range(epochs), desc="Training", colour='GREEN'):
+            self.training_strategy.before_epoch(epoch, self.model, self.p)
+
+            self.model.train()
+            epoch_train_loss = 0.0
+            epoch_train_error_real = 0.0
+            epoch_train_error_imag = 0.0
+
+            train_batch_processed = self.prepare_batch(train_batch)
+            outputs = self.model(train_batch_processed['xb'], train_batch_processed['xt'])
+
+            loss = self.training_strategy.compute_loss(outputs, train_batch_processed, self.model)
+
+            self.training_strategy.zero_grad(self.optimizers)
+            loss.backward()
+            self.training_strategy.step(self.optimizers)
+
+            errors = self.training_strategy.compute_errors(outputs, train_batch_processed, self.model)
+
+            epoch_train_loss += loss.item()
+            epoch_train_error_real += errors['real']
+            epoch_train_error_imag += errors['imag']
+
+            self.training_strategy.step_schedulers(self.schedulers)
+
+            self.evaluator.store_epoch_train_loss(epoch_train_loss)
+            self.evaluator.store_epoch_train_real_error(epoch_train_error_real)
+            self.evaluator.store_epoch_train_imag_error(epoch_train_error_imag)
+
+            if val_batch and self.training_strategy.can_validate():
+                self.model.eval()
+                epoch_val_loss = 0.0
+                epoch_val_error_real = 0.0
+                epoch_val_error_imag = 0.0
+
+                val_batch_processed = self.prepare_batch(val_batch)
+
+                with torch.no_grad():
+                    val_outputs = self.model(val_batch_processed['xb'], val_batch_processed['xt'])
+
+                    val_loss = self.training_strategy.compute_loss(val_outputs, val_batch_processed, self.model)
+                    epoch_val_loss += val_loss.item()
+
+                    val_errors = self.training_strategy.compute_errors(val_outputs, val_batch_processed, self.model)
+                    epoch_val_error_real += val_errors['real']
+                    epoch_val_error_imag += val_errors['imag']
+
+                self.evaluator.store_epoch_val_loss(epoch_val_loss)
+                self.evaluator.store_epoch_val_real_error(epoch_val_error_real)
+                self.evaluator.store_epoch_val_imag_error(epoch_val_error_imag)
+
+                if epoch_val_error_real < best_avg_error_real:
+                    best_avg_error_real = epoch_val_error_real
+                    best_model_checkpoint = {
+                        'model_state_dict': self.model.state_dict(),
+                    }
+                    self.saver.save_model(best_model_checkpoint, 'best_model.pth')
+
+                if epoch % 500 == 0:
+                    print(f"Epoch {epoch}: Train Loss: {epoch_train_loss:.3E}, Val Loss: {epoch_val_loss:.3E}, Val Error Real: {epoch_val_error_real:.3E}")
+
             else:
-                pred_real, pred_imag = self.model(xb=xb, xt=xt)
-            loss = loss_complex(g_u_real, g_u_imag, pred_real, pred_imag)
+                if epoch % 500 == 0:
+                    print(f"Epoch {epoch}: Train Loss: {epoch_train_loss:.3E}, Train Error Real: {epoch_train_error_real:.3E}, Train Error Imag: {epoch_train_error_imag:.3E}")
 
-        return loss.item(), pred_real, pred_imag
+            self.training_strategy.after_epoch(epoch, self.model, self.p)
 
-class TwoStepTrainer(ModelTrainer):
-    def __init__(self, model, optimizer, scheduler=None, training_phase='both'):
-        super().__init__(model, optimizer, scheduler)
-        self.training_phase = training_phase
+        end_time = time.time()
+        training_time = end_time - start_time
+        print(f"Training concluded in: {training_time:.2f} seconds")
 
-    def __call__(self, sample):
-        if self.training_phase == 'branch':
-            loss, coefs_real, coefs_imag, pred_real, pred_imag = self.train_step(sample)
-            outputs = {'loss': loss,
-                       'pred_real': pred_real,
-                       'pred_imag': pred_imag,
-                       'coefs_real': coefs_real,
-                       'coefs_imag': coefs_imag}
-        else:
-            loss, pred_real, pred_imag = self.train_step(sample)
-            outputs = {'loss': loss,
-                    'pred_real': pred_real,
-                    'pred_imag': pred_imag}
-        
-        return outputs
+        epochs_plot = list(range(epochs))
+        history = self.evaluator.get_history()
+        fig = plot_training(epochs_plot, history)
 
-    def train_step(self, sample):
-        self.model.train()
-        xb, xt = sample.get('xb'), sample.get('xt')
-        g_u_real, g_u_imag = sample['g_u_real'], sample['g_u_imag']
-
-        self.optimizer.zero_grad()
-
-        if self.training_phase == 'trunk':
-            pred_real, pred_imag = self.model(xt=xt)
-
-            loss = loss_complex(g_u_real, g_u_imag, pred_real, pred_imag)
-
-            loss.backward()
-            self.optimizer.step()
-
-            return loss.item(), pred_real, pred_imag
-
-        elif self.training_phase == 'branch':
-            coefs_real, coefs_imag, pred_real, pred_imag = self.model(xb=xb)
-
-            loss = loss_complex(coefs_real, coefs_imag, pred_real, pred_imag)
-            loss.backward()
-            self.optimizer.step()
-            
-            return loss.item(), coefs_real, coefs_imag, pred_real, pred_imag
-        
-        else:
-            pred_real, pred_imag = self.model(xb=xb, xt=xt)
-            loss = loss_complex(g_u_real, g_u_imag, pred_real, pred_imag)
-
-            return loss.item(), pred_real, pred_imag
+        self.saver.save_outputs(
+            model_state=best_model_checkpoint,
+            model_info=self.p,
+            split_indices=self.p['TRAIN_INDICES'],
+            norm_params=self.p['NORMALIZATION_PARAMETERS'],
+            history=history,
+            figure=fig,
+            time=training_time,
+            figure_prefix="training_history",
+            time_prefix="training"
+        )
