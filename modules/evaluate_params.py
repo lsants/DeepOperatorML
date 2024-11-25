669ae14c477c0c939a0919004a0df6890ee9a57e
ls <lssants7@gmail.com>
2024-11-25 07:15:03 -0500

    moved/deleted on refactoring

--- modules/evaluate_params.py
+++
@@ -1,3 +0,0 @@
-def multi_eval(v:tuple) -> tuple:
-    f = lambda x: eval(x) if type(x) == str else x
-    return tuple(map(f, v))
--- modules/loss_complex.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-def loss_complex(g_u_real, g_u_imag, pred_real, pred_imag):
-    loss_real = torch.mean((pred_real - g_u_real) ** 2)
-    loss_imag = torch.mean((pred_imag - g_u_imag) ** 2)
-    loss = loss_real + loss_imag
-
-    return loss
