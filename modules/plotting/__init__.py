2f6218ad824f7e4bf2353289d82ec1bd72bacf92
ls <lssants7@gmail.com>
2024-12-24 16:38:26 -0300

    plot fixing

--- modules/plotting/__init__.py
+++ modules/plotting/__init__.py
@@ -1,6 +1,6 @@
 # modules/plotting/__init__.py
 from .plot_axis import plot_axis
-from .plot_basis_function import plot_basis_function, plot_pod_basis
+from .plot_basis import plot_basis_function, plot_pod_basis
 from .plot_comparison import plot_axis_comparison, plot_field_comparison
 from .plot_field import plot_field
 from .plot_frequencies import plot_fft_field
@@ -8,7 +8,7 @@ from .plot_labels_axis import plot_labels_axis
 from .plot_training import plot_training
 
 __all__ = [plot_axis, 
-           plot_basis_function, 
+           plot_basis, 
            plot_pod_basis, 
            plot_axis_comparison, 
            plot_field_comparison, 
--- modules/plotting/plot_training.py
+++ modules/plotting/plot_training.py
@@ -18,6 +18,7 @@ def align_epochs(history):
         val_loss = metrics.get('val_loss', [])
         train_errors = metrics.get('train_errors', [])
         val_errors = metrics.get('val_errors', [])
+        learning_rate = metrics.get('learning_rate', [])
 
         # Extract real and imaginary errors if present
         train_errors_real = [e.get('g_u_real') for e in train_errors if isinstance(e, dict)]
@@ -36,6 +37,7 @@ def align_epochs(history):
         train_errors_imag = train_errors_imag + [None] * (num_epochs - len(train_errors_imag))
         val_errors_real = val_errors_real + [None] * (num_epochs - len(val_errors_real))
         val_errors_imag = val_errors_imag + [None] * (num_epochs - len(val_errors_imag))
+        learning_rate = learning_rate + [None] * (num_epochs - len(learning_rate))
 
         aligned_data[phase] = {
             'epochs': epochs,
@@ -45,6 +47,7 @@ def align_epochs(history):
             'train_errors_imag': train_errors_imag,
             'val_errors_real': val_errors_real,
             'val_errors_imag': val_errors_imag,
+            'learning_rate': learning_rate,
         }
 
     return aligned_data
@@ -61,9 +64,8 @@ def plot_training(history):
     n_phases = len(history)
     fig, ax = plt.subplots(nrows=n_phases, ncols=3, figsize=(15, 5 * n_phases))
 
-    # Ensure `ax` is always a 2D array for consistent indexing
     if n_phases == 1:
-        ax = [ax]  # Wrap single `Axes` object in a list for consistent indexing
+        ax = [ax]
 
     for i, (phase, metrics) in enumerate(history.items()):
         epochs = metrics['epochs']
@@ -73,6 +75,7 @@ def plot_training(history):
         train_errors_imag = metrics['train_errors_imag']
         val_errors_real = metrics['val_errors_real']
         val_errors_imag = metrics['val_errors_imag']
+        learning_rate = metrics['learning_rate']
 
         ax[i][0].plot(epochs[:len(train_loss)], train_loss, label='Train Loss', color='blue')
         if val_loss:
@@ -80,6 +83,10 @@ def plot_training(history):
         ax[i][0].set_title(f"Phase: {phase} - Loss")
         ax[i][0].set_yscale('log')
         ax[i][0].legend()
+        ax_i_0 = ax[i][0].twinx()
+        ax_i_0.plot(epochs[:len(train_loss)], learning_rate, label='Train Loss', color='black', linewidth=0.5)
+        ax_i_0.set_ylabel(r"learning_rate")
+        ax_i_0.set_yscale(r"log")
 
         if train_errors_real:
             ax[i][1].plot(epochs[:len(train_errors_real)], train_errors_real, label='Train Error (Real)', color='blue')
@@ -88,6 +95,10 @@ def plot_training(history):
         ax[i][1].set_title(f"Phase: {phase} - $L_2$ Error (Real)")
         ax[i][1].set_yscale('log')
         ax[i][1].legend()
+        ax_i_1 = ax[i][1].twinx()
+        ax_i_1.plot(epochs[:len(train_loss)], learning_rate, label='Train Loss', color='black', linewidth=0.5)
+        ax_i_1.set_ylabel(r"learning_rate")
+        ax_i_1.set_yscale(r"log")
 
         if train_errors_imag:
             ax[i][2].plot(epochs[:len(train_errors_imag)], train_errors_imag, label='Train Error (Imag)', color='blue')
@@ -95,7 +106,12 @@ def plot_training(history):
             ax[i][2].plot(epochs[:len(val_errors_imag)], val_errors_imag, label='Validation Error (Imag)', color='orange')
         ax[i][2].set_title(f"Phase: {phase} - $L_2$ Error (Imag)")
         ax[i][2].set_yscale('log')
+        ax_i_2 = ax[i][2].twinx()
         ax[i][2].legend()
+        ax_i_2 = ax[i][2].twinx()
+        ax_i_2.plot(epochs[:len(train_loss)], learning_rate, label='Train Loss', color='black', linewidth=0.5)
+        ax_i_2.set_ylabel(r"learning_rate")
+        ax_i_2.set_yscale(r"log")
 
     fig.tight_layout()
     return fig
