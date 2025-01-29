40f939e481b5ca16e70bf44b458dff248f97a0fc
ls <lssants7@gmail.com>
2025-01-29 02:30:01 -0300

    refactoring

--- modules/deeponet/output_strategies/__init__.py
+++ modules/deeponet/output_strategies/__init__.py
@@ -1,11 +1,13 @@
 from .single_trunk_split_branch import SingleTrunkSplitBranchStrategy
-from .multiple_trunks_split_branch import MultipleTrunksSplitBranchStrategy
+from .split_trunk_single_branch import SplitTrunkSingleBranchStrategy
+from .multiple_trunks_single_branch import MultipleTrunksSingleBranchStrategy
 from .single_trunk_multiple_branches import SingleTrunkMultipleBranchesStrategy
 from .multiple_trunks_multiple_branches import MultipleTrunksMultipleBranchesStrategy
 
 __all__ = [
     "SingleTrunkSplitBranchStrategy",
-    "MultipleTrunksSplitBranchStrategy",
+    "SplitTrunkSingleBranchStrategy",
+    "MultipleTrunksSingleBranchStrategy",
     "SingleTrunkMultipleBranchesStrategy",
     "MultipleTrunksMultipleBranchesStrategy",
 ]
