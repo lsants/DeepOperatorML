68b0edf341aad1e5480d422458efafb9dc213cc1
ls <lssants7@gmail.com>
2025-01-29 02:35:00 -0300

    refactoring

--- modules/plotting/__init__.py
+++ modules/plotting/__init__.py
@@ -1,6 +1,6 @@
 # modules/plotting/__init__.py
 from .plot_axis import plot_axis
-from .plot_basis import plot_basis_function, plot_pod_basis
+from .plot_basis import plot_basis_function
 from .plot_comparison import plot_axis_comparison, plot_field_comparison
 from .plot_field import plot_field
 from .plot_frequencies import plot_fft_field
@@ -9,7 +9,6 @@ from .plot_training import plot_training
 
 __all__ = [plot_axis, 
            plot_basis, 
-           plot_pod_basis, 
            plot_axis_comparison, 
            plot_field_comparison, 
            plot_field,  
