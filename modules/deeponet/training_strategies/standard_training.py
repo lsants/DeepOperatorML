341929e021964a90cab5cd9b33e0ce660e49e074
ls <lssants7@gmail.com>
2025-01-29 02:33:10 -0300

    refactoring

--- modules/deeponet/training_strategies/standard_training.py
+++ modules/deeponet/training_strategies/standard_training.py
@@ -10,17 +10,14 @@ class StandardTrainingStrategy(TrainingStrategy):
     def get_epochs(self, params):
         return [params['EPOCHS']]
 
-    def update_training_phase(self, phase):
-        if phase != 'default':
-            raise ValueError(f"Invalid phase for StandardTrainingStrategy: {phase}")
-        self.current_phase = phase
-
     def prepare_training(self, model, **kwargs):
-        for trunk in model.trunk_networks:
-            for param in trunk.parameters():
-                param.requires_grad = True
-        for branch in model.branch_networks:
-            for param in branch.parameters():
-                param.requires_grad = True
+        if hasattr(model, 'trunk_networks'):
+            for trunk in model.trunk_networks:
+                for param in trunk.parameters():
+                    param.requires_grad = True
+        if hasattr(model, 'branch_networks'):
+            for branch in model.branch_networks:
+                for param in branch.parameters():
+                    param.requires_grad = True
 
  
