60f47c9b507947b7ec03090dcd5f8dfc9d7fc55c
ls <lssants7@gmail.com>
2025-01-29 02:32:25 -0300

    implemented. yields mediocre results

--- modules/deeponet/output_strategies/single_trunk_split_branch.py
+++ modules/deeponet/output_strategies/single_trunk_split_branch.py
@@ -1,4 +1,4 @@
-# modules/model/output_strategies/single_trunk_single_branch.py
+# modules/model/output_strategies/single_trunk_split_branch.py
 
 import torch
 from .output_handling_base import OutputHandlingStrategy
@@ -10,6 +10,16 @@ class SingleTrunkSplitBranchStrategy(OutputHandlingStrategy):
     def __init__(self):
         self.branch_output_dim = None
         self.trunk_output_dim = None
+        
+    def get_basis_config(self):
+        """
+        Specifies that this strategy requires a single set of basis functions for both real and imaginary parts.
+
+        Returns:
+            dict: Basis configuration.
+        """
+        return {'type': 'single'}
+    
     def configure_networks(self, model, branch_config, trunk_config, **kwargs):
         """
         Configures the networks for the SingleTrunkSplitBranchStrategy.
@@ -18,21 +28,33 @@ class SingleTrunkSplitBranchStrategy(OutputHandlingStrategy):
             model (DeepONet): The model instance.
             branch_config (dict): Initial branch configuration.
             trunk_config (dict): Initial trunk configuration.
-            basis_functions (int): Number of basis functions to use.
+            pod_basis (torch.Tensor): Tensor with POD Basis computed from data. Optional (used only in POD).
 
         Returns:
             tuple: (branch_networks, trunk_networks) where both are ModuleLists.
         """
-        basis_functions = kwargs.get('basis_functions')
+        pod_basis = getattr(model, 'pod_basis', None)
+        n_basis_functions = model.n_basis_functions
+
+        if pod_basis is not None:
+            if pod_basis.shape[0] != 1:
+                raise ValueError("SingleTrunkSplitBranchStrategy expects a single set of basis functions with shape (1, n_features, n_modes).")
+            single_basis = pod_basis[0]
+            n_basis_functions = single_basis.shape[1]
+            model.n_basis_functions = n_basis_functions
+
         trunk_config = trunk_config.copy()
-        trunk_config['layers'][-1] = basis_functions
+        trunk_output_size = n_basis_functions
+        trunk_config['layers'].append(trunk_output_size)
 
         trunk = model.create_network(trunk_config)
         trunk_networks = torch.nn.ModuleList([trunk])
 
+        self.trunk_output_dim = trunk_output_size
+
         branch_config = branch_config.copy()
-        branch_output_size = basis_functions * model.n_outputs
-        branch_config['layers'][-1] = branch_output_size
+        branch_output_size = n_basis_functions * model.n_outputs
+        branch_config['layers'].append(branch_output_size)
 
         branch = model.create_network(branch_config)
         branch_networks = torch.nn.ModuleList([branch])
@@ -42,7 +64,6 @@ class SingleTrunkSplitBranchStrategy(OutputHandlingStrategy):
         return branch_networks, trunk_networks
     
     def forward(self, model, data_branch, data_trunk, matrices_branch=None, matrices_trunk=None):
-
         trunk_out = (
             matrices_trunk[0]
             if matrices_trunk is not None
@@ -58,8 +79,9 @@ class SingleTrunkSplitBranchStrategy(OutputHandlingStrategy):
         N = trunk_out.shape[-1] 
 
         outputs = []
+
         for i in range(model.n_outputs):
-            branch_out_split = branch_out[ i * N : (i + 1) * N , : ] 
+            branch_out_split = branch_out[ i * N : (i + 1) * N , : ]
             output = torch.matmul(trunk_out, branch_out_split).T
             outputs.append(output)
 
