28f0475e126cdba483ffe69d13f698fc181b31a7
ls <lssants7@gmail.com>
2025-01-29 02:29:48 -0300

    I think this doesn't change anything

--- modules/deeponet/optimization/loss_complex.py
+++ modules/deeponet/optimization/loss_complex.py
@@ -3,6 +3,6 @@ import torch
 def loss_complex(targets, preds):
     loss = 0
     for target, pred in zip(targets, preds):
-        loss += torch.mean((pred - target) ** 2)
+         loss += torch.mean((pred - target) ** 2)
 
     return loss
