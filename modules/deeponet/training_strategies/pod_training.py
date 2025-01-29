56d7405cd5cf6c42d3d2da81036c02513f2494b4
ls <lssants7@gmail.com>
2025-01-29 02:32:59 -0300

    working.

--- modules/deeponet/training_strategies/pod_training.py
+++ modules/deeponet/training_strategies/pod_training.py
@@ -1,37 +1,147 @@
 import torch
-from  .training_strategy_base import TrainingStrategy
+from .training_strategy_base import TrainingStrategy
 from ..optimization.loss_complex import loss_complex
 
-class PODTrainingStrategy(TrainingStrategy):
-    def __init__(self, pod_basis, mean_functions):
-        super().__init__()
-        self.pod_basis = pod_basis
-        self.mean_functions = mean_functions
 
-    def prepare_training(self, model):
+class PODTrainingStrategy(TrainingStrategy):
+    def __init__(self, data=None, var_share=None, inference=False):
+        super().__init__()
+        self.data = data
+        self.var_share = var_share
+        self.inference = inference
+        self.pod_basis = None
+        self.mean_functions = None
+        self.prepare_before_configure = True
+
+    def prepare_training(self, model, **kwargs):
         """
         Prepares the model for POD-based training by integrating POD basis and freezing the trunk networks.
 
         Args:
             model (DeepONet): The model instance.
         """
-        for i in range(model.n_outputs):
-            model.register_buffer(f'pod_basis_{i}', self.pod_basis[i])
-            model.register_buffer(f'mean_functions_{i}', self.mean_functions[i])
-        
-        for trunk in model.trunk_networks:
-            for param in trunk.parameters():
-                param.requires_grad = False
 
-    def update_training_phase(self, phase):
-        """
-        Not used here.
+        if self.pod_basis is None:
+            basis_config = kwargs.get("basis_config")
+
+            if basis_config is None:
+                raise ValueError(
+                    "'basis_config' must be provided by 'OutputHandlingStrategy'")
+
+            basis_type = basis_config.get('type')
+
+            if not self.inference:
+                if basis_type == 'single':
+                    num_basis = self._prepare_single_basis(model)
+                    model.n_basis_functions = num_basis
+                elif basis_type == 'multiple':
+                    num_basis = self._prepare_multiple_basis(model)
+                    model.n_basis_functions = num_basis
+                else:
+                    raise ValueError(
+                        f"Unknown 'basis_config' type: {basis_type}")
+
+    def _prepare_single_basis(self, model, **kwargs):
+        """Computes a single set of POD basis functions for all outputs.
 
         Args:
-            model (DeepONet): The model instance.
-            phase (str): Not used in POD training.
+            model (DeepONet): Model instance.
         """
-        pass
+
+        if self.data is None:
+            raise ValueError("Data must be provided for PODTrainingStrategy.")
+
+        variance_share = self.var_share
+        pod_basis_list = []
+        mean_functions_list = []
+
+        # Data is a dict with cols 'xb', 'xt', 'g_u_real', 'g_u_imag', 'index'
+        output_hashes = [k for k in self.data.keys() if k not in [
+            'xb', 'xt', 'index']]
+
+        # Reshape ouputs to shape (n_outputs * input_functions, coordinates)
+        outputs = torch.Tensor()
+        for output in output_hashes:
+            outputs = torch.cat((outputs, self.data[output]), 0)
+
+        mean = torch.mean(outputs, dim=0)
+        mean_functions_list.append(mean)
+        centered = (outputs - mean).T
+
+        U, S, _ = torch.linalg.svd(centered)
+        explained_variance_ratio = torch.cumsum(
+            S**2, dim=0) / torch.linalg.norm(S)**2
+
+        if variance_share:
+            most_significant_modes = (
+                explained_variance_ratio < variance_share).sum() + 1
+        else:
+            raise ValueError(
+                "Variance share was not given. There's no way to know how many modes should be used.")
+
+        num_modes = most_significant_modes
+        basis = U[:, : num_modes]
+        pod_basis_list.append(basis)
+
+        self.pod_basis = torch.stack(pod_basis_list, dim=0)
+
+        self.mean_functions = torch.stack(mean_functions_list, dim=0)
+
+        model.register_buffer(f'pod_basis', self.pod_basis)
+        model.register_buffer(f'mean_functions', self.mean_functions)
+
+        return num_modes
+
+    def _prepare_multiple_basis(self, model, **kwargs):
+        """Computes multiple sets of POD basis functions, one for each output.
+
+        Args:
+            model (DeepONet): Model instance.
+            data (torch.utils.data.Subset): Training data.
+        """
+
+        if self.data is None:
+            raise ValueError("Data must be provided for PODTrainingStrategy.")
+
+        output_keys = [i for i in self.data.keys() if i not in ['xb', 'xt', 'index']]
+        outputs_names = output_keys
+        variance_share = self.var_share
+        pod_basis_list = []
+        mean_functions_list = []
+
+        for output_name in outputs_names:
+            output = self.data[output_name]
+
+            mean = torch.mean(output, dim=0)
+            mean_functions_list.append(mean)
+            centered = (output - mean).T
+
+            U, S, _ = torch.linalg.svd(centered)
+            explained_variance_ratio = torch.cumsum(
+                S ** 2, dim=0) / torch.linalg.norm(S) ** 2
+            if variance_share:
+                most_significant_modes = (
+                    explained_variance_ratio < variance_share).sum() + 1
+            else:
+                raise ValueError(
+                    "Variance share was not given. There's no way to know how many modes should be used.")
+
+            num_modes = most_significant_modes
+            basis = U[:, : num_modes]
+            pod_basis_list.append(basis)
+
+        self.pod_basis = torch.stack(pod_basis_list, dim=0)
+        self.mean_functions = torch.stack(mean_functions_list, dim=0)
+
+        model.register_buffer(f'pod_basis', self.pod_basis)
+        model.register_buffer(f'mean_functions', self.mean_functions)
+
+    def get_basis_functions(self):
+        return self.pod_basis
+
+    def set_basis(self, pod_basis, mean_functions):
+        self.pod_basis = pod_basis
+        self.mean_functions = mean_functions
 
     def get_trunk_output(self, model, i, xt_i):
         """
@@ -45,8 +155,12 @@ class PODTrainingStrategy(TrainingStrategy):
         Returns:
             torch.Tensor: Trunk output for the i-th output, which is pod_basis.
         """
-        return getattr(model, f'pod_basis_{i}')
-    
+
+        pod_basis = self.pod_basis
+        requested_basis = pod_basis[i]
+
+        return requested_basis
+
     def get_branch_output(self, model, i, xb_i):
         """
         Optionally, modify the branch output if needed. For POD, branches are used as is.
@@ -59,5 +173,14 @@ class PODTrainingStrategy(TrainingStrategy):
         Returns:
             torch.Tensor: Branch output for the i-th output.
         """
-        return model.branch_networks[i](xb_i)
-    
+
+        return model.branch_networks[i](xb_i).T
+
+    def forward(self, model, xb=None, xt=None):
+        pod_basis = self.pod_basis
+        return model.output_strategy.forward(model, data_branch=xb, data_trunk=pod_basis)
+
+    def after_epoch(self, epoch, model, params, **kwargs):
+        if epoch > 1:
+            return
+        params['BASIS_FUNCTIONS'] = model.n_basis_functions
