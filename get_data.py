9acc8e66cf4603e970598be6acf994cee5137139
ls <lssants7@gmail.com>
2025-02-25 03:52:24 -0300

    modified kelvin data

--- get_data.py
+++ get_data.py
@@ -15,61 +15,88 @@ from modules.data_generation.data_generation_kelvin import KelvinsProblemDetermi
 
 logger = logging.getLogger(__name__)
 
-parser = argparse.ArgumentParser()
 
-parser.add_argument("problem", type=str, help="Generate data for given problem")
-args = parser.parse_args()
+def gen_data(config_path: str):
+    parser = argparse.ArgumentParser()
 
-problem = args.problem.lower()
+    parser.add_argument("problem", type=str, help="Generate data for given problem")
+    args = parser.parse_args()
 
-with open('params_data_generation.yaml') as file:
-    p = yaml.safe_load(file)
+    problem = args.problem.lower()
 
-np.random.seed(p["SEED"])
+    with open(config_path) as file:
+        p = yaml.safe_load(file)
 
-filename = os.path.join(f"{p['DATA_FILENAME']}")
+    np.random.seed(p["SEED"])
 
-def to_float(val):
-    try:
-        return float(val)
-    except (ValueError, TypeError):
-        return val
+    filename = os.path.join(f"{p['DATA_FILENAME']}")
 
+    # --------- Grouping parameters -------------
 
+    if problem == "dynamic_fixed_material":
+        data_size = (p["N"], 
+                     p["N_R"], 
+                     p["N_Z"])
+        load_params = ((p["OMEGA_MAX"]), # Gotta fix this, should evaluate
+                       (p["OMEGA_MIN"]), 
+                       (p["LOAD"]), 
+                       (p["Z_SOURCE"]), 
+                       (p["L_SOURCE"]), 
+                       (p["R_SOURCE"]))
+        mesh_params = ((p["R_MIN"], 
+                        p["R_MAX"], 
+                        p["Z_MIN"], 
+                        p["Z_MAX"]))
+        problem_setup = (p['COMPONENT'], 
+                         p['LOADTYPE'], 
+                         p['BVPTYPE'])
+        material_params = ((p["E"], 
+                            p["NU"], 
+                            p["DAMP"], 
+                            p["DENS"]))
 
-# --------- Grouping parameters -------------
+        influence_functions = DynamicFixedMaterialProblem(
+            data_size,
+            material_params,
+            load_params,
+            mesh_params,
+            problem_setup,
+        )
+        influence_functions.produce_samples(filename)
 
-if problem == "dynamic_fixed_material":
-    data_size = (p["N"], p["N_R"], p["N_Z"])
-    load_params = ((p["OMEGA_MAX"]), (p["OMEGA_MIN"]), (p["LOAD"]), (p["Z_SOURCE"]), (p["L_SOURCE"]), (p["R_SOURCE"]))
-    mesh_params = ((p["R_MIN"], p["R_MAX"], p["Z_MIN"], p["Z_MAX"]))
-    problem_setup = (p['COMPONENT'], p['LOADTYPE'], p['BVPTYPE'])
-    material_params = ((p["E"], p["NU"], p["DAMP"], p["DENS"]))
+    elif problem == "kelvin":
+        data_size = (p["N_F_KELVIN"], 
+                     p["N_MU_KELVIN"], 
+                     p["N_NU_KELVIN"], 
+                     p["N_X_KELVIN"], 
+                     p["N_Y_KELVIN"], 
+                     p["N_Z_KELVIN"])
+        material_params = (p["MU_MIN_KELVIN"], 
+                           p["MU_MAX_KELVIN"], 
+                           p["NU_MIN_KELVIN"], 
+                           p["NU_MAX_KELVIN"])
+        load_params = ((p["F_MIN_KELVIN"], 
+                        p["F_MAX_KELVIN"]))
+        mesh_params = ((p["X_MIN_KELVIN"], 
+                        p["X_MAX_KELVIN"], 
+                        p["Y_MIN_KELVIN"], 
+                        p["Y_MAX_KELVIN"], 
+                        p["Z_MIN_KELVIN"], 
+                        p["Z_MAX_KELVIN"]))
+        problem_setup = p["LOAD_DIRECTION_KELVIN"]
 
-    influence_functions = DynamicFixedMaterialProblem(
-        data_size,
-        material_params,
-        load_params,
-        mesh_params,
-        problem_setup,
-    )
-    influence_functions.produce_samples(filename)
+        influence_functions = KelvinsProblemDeterministic(
+            data_size,
+            material_params,
+            load_params,
+            mesh_params,
+            problem_setup
+        )
+        influence_functions.produce_samples(filename)
 
-elif problem == "kelvin":
-    data_size = (p["N_KELVIN"], p["N_X_KELVIN"], p["N_Y_KELVIN"], p["N_Z_KELVIN"])
-    material_params = (p["MU_KELVIN"], p["NU_KELVIN"])
-    load_params = ((p["F_MIN_KELVIN"], p["F_MAX_KELVIN"]))
-    mesh_params = ((p["X_MIN_KELVIN"], p["X_MAX_KELVIN"], p["Y_MIN_KELVIN"], p["Y_MAX_KELVIN"], p["Z_MIN_KELVIN"], p["Z_MAX_KELVIN"]))
-    problem_setup = p["LOAD_DIRECTION_KELVIN"]
+    else:
+        print("fatal error: not a valid problem.", file=sys.stderr)
 
-    influence_functions = KelvinsProblemDeterministic(
-        data_size,
-        material_params,
-        load_params,
-        mesh_params,
-        problem_setup
-    )
-    influence_functions.produce_samples(filename)
+if __name__ == "__main__":
 
-else:
-    print("fatal error: not a valid problem.", file=sys.stderr)
+    gen_data("./configs/config_data_generation.yaml")
