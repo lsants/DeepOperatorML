95b8bb312655db767d74c2f428a3077417760938
ls <lssants7@gmail.com>
2025-02-26 04:00:15 -0300

    moved activation functions to a file

---
+++ modules/deeponet/nn/activation_fns.py
@@ -0,0 +1,12 @@
+import torch
+
+ACTIVATION_MAP = {
+    'relu': torch.nn.ReLU(),
+    'tanh': torch.nn.Tanh(),
+    'sigmoid': torch.nn.Sigmoid(),
+    'leaky_relu': torch.nn.LeakyReLU(),
+    'elu': torch.nn.ELU(),
+    'gelu': torch.nn.GELU(),
+    'softplus': torch.nn.Softplus(),
+    'identity': torch.nn.Identity()
+}
