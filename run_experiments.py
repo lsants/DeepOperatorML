d7444d375b2c308b47e48ed8ae97aa4e79cccea0
ls <lssants7@gmail.com>
2025-02-27 17:57:22 -0300

    teste multi basis trunk

--- run_experiments.py
+++ run_experiments.py
@@ -1,35 +1,58 @@
 import os
+import sys
+import time
 import subprocess
 import logging
 import yaml
+
+logging.basicConfig(
+    # filename=f"./data/logs/{time.strftime('%Y%m%D%H%M%S')}_main.log",
+    filemode='w',
+    level=logging.DEBUG,
+    format="[%(asctime)s] [%(levelname)s] %(name)s: %(message)s",
+    datefmt="%d-%m-%Y %H:%M:%S",
+    stream=sys.stdout
+)
 
 logger = logging.getLogger(__name__)
 
 BASE_CONFIG = 'configs/config_train.yaml'
 
 overrides_list = [
-    {"PROBLEM": "dynamic", 
-     "DATAFILE" : "./data/raw/displacements.npz", 
+    {"PROBLEM": "dynamic", # perfect
+     "DATAFILE" : "./data/raw/displacements.npz",
      "INPUT_FUNCTION_KEYS" : ["xb"],
      "COORDINATE_KEYS" : ["r", "z"],
      "OUTPUT_KEYS" : ["g_u_real", "g_u_imag"],
-     "TRAINING_STRATEGY": "two_step", 
-     "OUTPUT_HANDLING": "single_trunk_split_branch",
+     "TRAINING_STRATEGY": "two_step",
+     "OUTPUT_HANDLING": "share_trunk",
      "TRUNK_DECOMPOSITION" : "svd"},
 
-    {"PROBLEM": "dynamic", 
-     "DATAFILE" : "./data/raw/displacements.npz", 
-     "INPUT_FUNCTION_KEYS" : ["xb"],
-     "COORDINATE_KEYS" : ["r", "z"],
-     "OUTPUT_KEYS" : ["g_u_real", "g_u_imag"],
-     "TRAINING_STRATEGY": "two_step", 
-     "OUTPUT_HANDLING": "single_trunk_split_branch",
-     "TRUNK_DECOMPOSITION" : "qr"}
+    # {"PROBLEM": "dynamic",
+    #  "DATAFILE" : "./data/raw/displacements.npz",
+    #  "INPUT_FUNCTION_KEYS" : ["xb"],
+    #  "COORDINATE_KEYS" : ["r", "z"],
+    #  "OUTPUT_KEYS" : ["g_u_real", "g_u_imag"],
+    #  "TRAINING_STRATEGY": "two_step",
+    #  "OUTPUT_HANDLING": "share_branch",
+    #  "TRUNK_DECOMPOSITION" : "svd"},
+
+    # {"PROBLEM": "dynamic",
+    #  "DATAFILE": "./data/raw/displacements.npz",
+    #  "INPUT_FUNCTION_KEYS": ["xb"],
+    #  "COORDINATE_KEYS": ["r", "z"],
+    #  "OUTPUT_KEYS": ["g_u_real", "g_u_imag"],
+    #  "TRAINING_STRATEGY": "two_step",
+    #  "OUTPUT_HANDLING": "split_networks",
+    #  "TRUNK_DECOMPOSITION": "svd"},
+
 ]
+
 
 def run_experiments():
     for i, overrides in enumerate(overrides_list, start=1):
-        logger.info(f"\n======================== RUN {i} with overrides: {overrides} =======================\n")
+        logger.info(
+            f"\n======================== RUN {i} with overrides: {overrides} =======================\n")
 
         with open(BASE_CONFIG, 'r') as file:
             config_data = yaml.safe_load(file)
@@ -40,10 +63,12 @@ def run_experiments():
         temp_config_path = f"temp_config_{i}.yaml"
         with open(temp_config_path, 'w') as file:
             yaml.safe_dump(config_data, file)
-        
-        subprocess.run(["python3", "main.py", "--train-config", temp_config_path], check=True)
+
+        subprocess.run(["python3", "main.py", "--train-config",
+                       temp_config_path], check=True)
 
         os.remove(temp_config_path)
+
 
 if __name__ == "__main__":
     run_experiments()
