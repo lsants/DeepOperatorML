e5e913926e3c698781a150fe4d87bb6ccf09f04a
ls <lssants7@gmail.com>
2025-02-27 18:00:51 -0300

    changed basis naming

--- modules/utilities/config_utils.py
+++ modules/utilities/config_utils.py
@@ -1,20 +1,21 @@
 import os
 from datetime import datetime
 
+
 def process_config(config):
     """
     Processes the configuration dictionary using the "PROBLEM" key to adjust the model name and paths.
     Raises a ValueError if any required key (MODELNAME, PROBLEM, TRAINING_STRATEGY, OUTPUT_HANDLING,
     DATAFILE, OUTPUT_LOG_FOLDER, or IMAGES_FOLDER) is missing or None.
-    
+
     The function modifies:
       - MODELNAME: Appends the problem name and additional tags.
       - OUTPUT_LOG_FOLDER and IMAGES_FOLDER: Adjusts these by including the problem, training strategy,
         and output handling information.
-    
+
     Args:
         config (dict): The raw configuration dictionary loaded from YAML.
-    
+
     Returns:
         dict: The modified configuration dictionary.
     """
@@ -48,19 +49,21 @@ def process_config(config):
     if config.get("TRUNK_FEATURE_EXPANSION", False):
         model_name += "_trunkexp"
     output_handling = config["OUTPUT_HANDLING"].lower()
-    if "single_trunk" in output_handling:
-        model_name += "_singlebasis"
-    elif "multiple_trunks" in output_handling:
-        model_name += "_multitrunks"
-    elif "split_trunk" in output_handling:
-        model_name += "_splitbasis"
+    if "share_trunk" in output_handling:
+        model_name += "_single_basis"
+    if "share_branch" in output_handling:
+        model_name += "_mult_basis"
+    elif "split_networks" in output_handling:
+        model_name += "_split"
 
     config["MODELNAME"] = model_name
 
     model_folder = config["MODEL_FOLDER"]
     training_strategy_str = config["TRAINING_STRATEGY"]
     output_handling_str = config["OUTPUT_HANDLING"]
-    config["MODEL_FOLDER"] = os.path.join(model_folder, problem, training_strategy_str, output_handling_str, model_name) + os.sep
-    config["IMAGES_FOLDER"] = os.path.join(config["MODEL_FOLDER"], 'images') + os.sep
+    config["MODEL_FOLDER"] = os.path.join(
+        model_folder, problem, training_strategy_str, output_handling_str, model_name) + os.sep
+    config["IMAGES_FOLDER"] = os.path.join(
+        config["MODEL_FOLDER"], 'images') + os.sep
 
     return config
