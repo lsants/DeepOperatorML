e6425e5c9f02c7cb9b1e676752ee4a95c6c0f833
leo <l201292@dac.unicamp.br>
2025-03-02 20:24:48 -0300

    finished implementation for pod and two step

--- modules/deeponet/output_strategies/split_networks.py
+++ modules/deeponet/output_strategies/split_networks.py
@@ -18,10 +18,7 @@ class SplitNetworksStrategy(OutputHandlingStrategy):
         n_basis_functions = model.n_basis_functions
 
         if pod_basis is not None:
-            if pod_basis.shape[0] < 2:
-                raise ValueError(
-                    "Split networks strategy expects multiple sets of basis functions with shape (n_basis_sets, n_features, n_modes), with n_basis_sets > 1.")
-            n_basis_functions = pod_basis.shape[1]
+            n_basis_functions = pod_basis.shape[-1] // model.n_outputs
             model.n_basis_functions = n_basis_functions
 
         self.trunk_output_size = n_basis_functions * model.n_outputs
@@ -38,24 +35,31 @@ class SplitNetworksStrategy(OutputHandlingStrategy):
         
         branch_network = model.create_network(branch_config)
 
-        logger.info(f"\nNumber of Branch outputs: {self.n_branch_outputs}\n")
-        logger.info(f"\nNumber of Trunk outputs: {self.n_trunk_outputs}\n")
-        logger.info(
+        logger.debug(f"\nNumber of Branch outputs: {self.n_branch_outputs}\n")
+        logger.debug(f"\nNumber of Trunk outputs: {self.n_trunk_outputs}\n")
+        logger.debug(
             f"\nBranch layer sizes: {pprint_layer_dict(branch_config['layers'])}\n")
-        logger.info(
+        logger.debug(
             f"\nTrunk layer sizes: {pprint_layer_dict(trunk_config['layers'])}\n")
 
-        logger.info(
+        logger.debug(
             f"\nBranch network size: {self.branch_output_size}\nTrunk network size: {self.trunk_output_size}\n")
 
         return branch_network, trunk_network
 
     def forward(self, model, data_branch, data_trunk, matrix_branch=None, matrix_trunk=None):
+        N = model.n_basis_functions
+        
         branch_out = (
             matrix_branch
             if matrix_branch is not None
             else model.get_branch_output(data_branch)
         )
+        if data_trunk is None and matrix_trunk is None:
+            identity = torch.eye(N).to(dtype=branch_out.dtype, 
+                                   device=branch_out.device)
+            I_generator = (identity for _ in range(model.n_outputs))
+            matrix_trunk = torch.cat(tuple(I_generator), dim=1)
         trunk_out = (
             matrix_trunk
             if matrix_trunk is not None
@@ -64,7 +68,6 @@ class SplitNetworksStrategy(OutputHandlingStrategy):
         outputs = []
 
         for i in range(model.n_outputs):
-            N = model.n_basis_functions
             trunk_out_split = trunk_out[ :, i * N: (i + 1) * N]
             branch_out_split = branch_out[i * N: (i + 1) * N, : ]
             output = torch.matmul(trunk_out_split, branch_out_split).T
