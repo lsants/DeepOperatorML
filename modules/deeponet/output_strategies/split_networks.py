0e31a533751703577513467dff16c8c250392fff
leo <l201292@dac.unicamp.br>
2025-03-02 02:51:27 -0300

    split networks strategy

--- modules/deeponet/output_strategies/split_networks.py
+++ modules/deeponet/output_strategies/split_networks.py
@@ -8,10 +8,7 @@ logger = logging.getLogger(__name__)
 
 class SplitNetworksStrategy(OutputHandlingStrategy):
     def __init__(self):
-        self.branch_output_size = None
-        self.trunk_output_size = None
-        self.num_trunks = None
-        self.num_branches = None
+        super().__init__()
 
     def get_basis_config(self):
         return {'type': 'multiple'}
@@ -27,55 +24,49 @@ class SplitNetworksStrategy(OutputHandlingStrategy):
             n_basis_functions = pod_basis.shape[1]
             model.n_basis_functions = n_basis_functions
 
+        self.trunk_output_size = n_basis_functions * model.n_outputs
+        self.n_trunk_outputs = self.trunk_output_size // model.n_basis_functions
         trunk_config = trunk_config.copy()
-        trunk_output_size = n_basis_functions * model.n_outputs
-        trunk_config['layers'].append(trunk_output_size)
+        trunk_config['layers'].append(self.trunk_output_size)
+        
+        trunk_network = model.create_network(trunk_config)
 
-        trunk = model.create_network(trunk_config)
-        trunk_networks = torch.nn.ModuleList([trunk])
-
-        self.num_trunks = len(trunk_networks)
-        self.trunk_output_size = trunk_output_size
-
+        self.branch_output_size = self.trunk_output_size
+        self.n_branch_outputs = self.n_trunk_outputs
         branch_config = branch_config.copy()
-        branch_output_size = n_basis_functions * model.n_outputs
-        branch_config['layers'].append(branch_output_size)
+        branch_config['layers'].append(self.branch_output_size)
+        
+        branch_network = model.create_network(branch_config)
 
-        branch = model.create_network(branch_config)
-        branch_networks = torch.nn.ModuleList([branch])
-
-        self.num_branches = len(branch_networks)
-        self.branch_output_size = branch_output_size
-
-        logger.info(f"\nNumber of Branch networks: {self.num_branches}\n")
-        logger.info(f"\nNumber of Trunk networks: {self.num_trunks}\n")
+        logger.info(f"\nNumber of Branch outputs: {self.n_branch_outputs}\n")
+        logger.info(f"\nNumber of Trunk outputs: {self.n_trunk_outputs}\n")
         logger.info(
             f"\nBranch layer sizes: {pprint_layer_dict(branch_config['layers'])}\n")
         logger.info(
             f"\nTrunk layer sizes: {pprint_layer_dict(trunk_config['layers'])}\n")
 
         logger.info(
-            f"\nBranch network size: {branch_output_size}\nTrunk network size: {trunk_output_size}\n")
+            f"\nBranch network size: {self.branch_output_size}\nTrunk network size: {self.trunk_output_size}\n")
 
-        return branch_networks, trunk_networks
+        return branch_network, trunk_network
 
-    def forward(self, model, data_branch, data_trunk, matrices_branch=None, matrices_trunk=None):
+    def forward(self, model, data_branch, data_trunk, matrix_branch=None, matrix_trunk=None):
         branch_out = (
-            matrices_branch[0]
-            if matrices_branch is not None
-            else model.get_branch_output(0, data_branch)
+            matrix_branch
+            if matrix_branch is not None
+            else model.get_branch_output(data_branch)
         )
         trunk_out = (
-            matrices_trunk[0]
-            if matrices_trunk is not None
-            else model.get_trunk_output(0, data_trunk)
+            matrix_trunk
+            if matrix_trunk is not None
+            else model.get_trunk_output(data_trunk)
         )
         outputs = []
 
         for i in range(model.n_outputs):
             N = model.n_basis_functions
-            trunk_out_split = trunk_out[:, i * N: (i + 1) * N]
-            branch_out_split = branch_out[i * N: (i + 1) * N, :]
+            trunk_out_split = trunk_out[ :, i * N: (i + 1) * N]
+            branch_out_split = branch_out[i * N: (i + 1) * N, : ]
             output = torch.matmul(trunk_out_split, branch_out_split).T
             outputs.append(output)
 
