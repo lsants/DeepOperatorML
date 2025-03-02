e4f57a3de2a69bf8d457e8b561dac4dd89cbb88f
leo <l201292@dac.unicamp.br>
2025-03-02 02:52:42 -0300

    changed from nn module list to just network in architecture => rewrite requires grad loop

--- modules/deeponet/training_strategies/standard_training.py
+++ modules/deeponet/training_strategies/standard_training.py
@@ -10,13 +10,11 @@ class StandardTrainingStrategy(TrainingStrategy):
         return [params['EPOCHS']]
 
     def prepare_training(self, model, **kwargs):
-        if hasattr(model, 'trunk_networks'):
-            for trunk in model.trunk_networks:
-                for param in trunk.parameters():
-                    param.requires_grad = True
-        if hasattr(model, 'branch_networks'):
-            for branch in model.branch_networks:
-                for param in branch.parameters():
-                    param.requires_grad = True
+        if hasattr(model, 'trunk_network'):
+            for param in model.trunk_network.parameters():
+                param.requires_grad = True
+        if hasattr(model, 'branch_network'):
+            for param in model.branch_network.parameters():
+                param.requires_grad = True
 
  
