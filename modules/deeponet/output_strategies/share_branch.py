6d412cee0fa83a715549492b32fafecafc02dd88
leo <l201292@dac.unicamp.br>
2025-03-01 23:10:40 -0300

    share branch strategy (works but bad results)

--- modules/deeponet/output_strategies/share_branch.py
+++ modules/deeponet/output_strategies/share_branch.py
@@ -12,10 +12,7 @@ class ShareBranchStrategy(OutputHandlingStrategy):
     """
 
     def __init__(self):
-        self.branch_output_size = None
-        self.trunk_output_size = None
-        self.num_trunks = None
-        self.num_branches = None
+        super().__init__()
 
     def get_basis_config(self):
         """
@@ -37,7 +34,7 @@ class ShareBranchStrategy(OutputHandlingStrategy):
             pod_basis (torch.Tensor): Tensor with POD Basis computed from data. Optional (used only in POD).
 
         Returns:
-            tuple: (branch_networks, trunk_networks) where both are ModuleLists.
+            tuple: (branch_network, trunk_network) where both are ModuleLists.
         """
         pod_basis = getattr(model, 'pod_basis', None)
         n_basis_functions = model.n_basis_functions
@@ -49,65 +46,55 @@ class ShareBranchStrategy(OutputHandlingStrategy):
             n_basis_functions = pod_basis.shape[-1]
             model.n_basis_functions = n_basis_functions
 
+        self.trunk_output_size = n_basis_functions * model.n_outputs
+        self.n_trunk_outputs = self.trunk_output_size // model.n_basis_functions
         trunk_config = trunk_config.copy()
-        trunk_output_size = n_basis_functions * model.n_outputs
-        trunk_config['layers'].append(trunk_output_size)
+        trunk_config['layers'].append(self.trunk_output_size)
 
-        trunk = model.create_network(trunk_config)
-        trunk_networks = torch.nn.ModuleList([trunk])
+        trunk_network = model.create_network(trunk_config)
 
-        self.trunk_output_size = trunk_output_size
-        self.num_trunks = len(trunk_networks)
-
+        self.branch_output_size = n_basis_functions
+        self.n_branch_outputs = self.branch_output_size // model.n_basis_functions
         branch_config = branch_config.copy()
-        branch_output_size = n_basis_functions
-        branch_config['layers'].append(branch_output_size)
+        branch_config['layers'].append(self.branch_output_size)
 
-        branch = model.create_network(branch_config)
-        branch_networks = torch.nn.ModuleList([branch])
+        branch_network = model.create_network(branch_config)
 
-        self.branch_output_size = branch_output_size
-        self.num_branches = len(branch_networks)
-
-        logger.info(f"\nNumber of Branch networks: {self.num_branches}\n")
-        logger.info(f"\nNumber of Trunk networks: {self.num_trunks}\n")
+        logger.info(f"\nNumber of Branch outputs: {self.n_branch_outputs}\n")
+        logger.info(f"\nNumber of Trunk outputs: {self.n_trunk_outputs}\n")
         logger.info(
             f"\nBranch layer sizes: {pprint_layer_dict(branch_config['layers'])}\n")
         logger.info(
             f"\nTrunk layer sizes: {pprint_layer_dict(trunk_config['layers'])}\n")
 
         logger.info(
-            f"\nBranch network size: {branch_output_size}\nTrunk network size: {trunk_output_size}\n")
+            f"\nBranch network size: {self.branch_output_size}\nTrunk network size: {self.trunk_output_size}\n")
 
-        return branch_networks, trunk_networks
+        return branch_network, trunk_network
 
-    def forward(self, model, data_branch, data_trunk, matrices_branch=None, matrices_trunk=None):
+    def forward(self, model, data_branch, data_trunk, matrix_branch=None, matrix_trunk=None):
         N = model.n_basis_functions
 
         branch_out = (
-            matrices_branch[0]
-            if matrices_branch is not None
-            else model.get_branch_output(0, data_branch)
+            matrix_branch
+            if matrix_branch is not None
+            else model.get_branch_output(data_branch)
         )
 
-        if data_trunk is None and matrices_trunk is None:
+        if data_trunk is None and matrix_trunk is None:
             identity = torch.eye(N).to(dtype=branch_out.dtype, 
                                    device=branch_out.device)
             I_generator = (identity for _ in range(model.n_outputs))
-            matrices_trunk = [torch.cat(tuple(I_generator), dim=1)]
+            matrix_trunk = [torch.cat(tuple(I_generator), dim=1)]
 
-        print("THIS SHOULD RUN 3", matrices_trunk[0].shape if matrices_trunk is not None else data_trunk)
+        # print("THIS SHOULD RUN 3", matrix_trunk.shape if matrix_trunk is not None else data_trunk)
         trunk_out = (
-            matrices_trunk[0]
-            if matrices_trunk is not None
-            else model.get_trunk_output(0, data_trunk)
+            matrix_trunk
+            if matrix_trunk is not None
+            else model.get_trunk_output(data_trunk)
         )
 
-        print("THIS SHOULD RUN 4", trunk_out.shape)
-
-        print("THIS SHOULD RUN 5", trunk_out.shape)
-
-
+        # print("THIS SHOULD RUN 4", trunk_out.shape)
 
         outputs = []
         
@@ -118,6 +105,6 @@ class ShareBranchStrategy(OutputHandlingStrategy):
                 trunk_out = identity
             output = torch.matmul(trunk_out_split, branch_out).T
             outputs.append(output)
-        print("THIS SHOULD RUN 6", trunk_out.shape)
+        # print("THIS SHOULD RUN 6", trunk_out.shape)
 
         return tuple(outputs)
