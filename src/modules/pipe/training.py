2a604f109ef9a37fb170f73a5b3a02caadab87c2
201202 <l201202@dac.unicamp.br>
2025-03-11 12:44:22 -0300

    print for kaggle

--- src/modules/pipe/training.py
+++ src/modules/pipe/training.py
@@ -89,7 +89,7 @@ class TrainingLoop:
                 loss = self.training_strategy.compute_loss(outputs, batch, self.model, self.training_params)
 
                 if epoch % 100 == 0 and epoch > 0:
-                    logger.info(f"Loss: {loss:.2E}")
+                    print(f"Loss: {loss:.2E}")
 
                 active_optimizer.zero_grad()
                 loss.backward()
